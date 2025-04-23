9f4a156e5d98aa6dde59dd7c1d3a51d7ef00b8a5
RAKHIKRISHNAAR <rakhykrishnaa5ar@gmail.com>
2025-04-23 11:38:28 +0530

    rote setting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Home from './pages/Home'
 
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
 
 const App = () => {
   return (
-    <main className='bg-black'>
-   <Home/>
-      
-    </main>
-    )
-}
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+  );
+};
 
-export default App
+export default App;
